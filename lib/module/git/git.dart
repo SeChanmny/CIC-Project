4b44f57616e0eee26c2909d370c82f418abb69b8
Se Chanmny <pozzmny123@gmail.com>
2023-01-17 11:52:49 +0700

    update Container

--- lib/module/git/git.dart
+++ lib/module/git/git.dart
@@ -1,11 +1,14 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class Git extends StatelessWidget {
   const Git({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.red,
+    );
   }
 }
